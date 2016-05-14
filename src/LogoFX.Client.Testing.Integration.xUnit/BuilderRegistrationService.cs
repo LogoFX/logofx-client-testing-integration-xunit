a9d90b8042328d9a170d9aef982564fedbee7fef
godrose <godrose@gmail.com>
2016-05-14 16:50:48 +0300

    Added Universal Windows Platform project.

--- src/LogoFX.Client.Testing.Integration.xUnit/BuilderRegistrationService.cs
+++ src/LogoFX.Client.Testing.Integration.xUnit/BuilderRegistrationService.cs
@@ -5,7 +5,7 @@ using LogoFX.Client.Testing.Contracts;
 namespace LogoFX.Client.Testing.Integration.xUnit
 {
     /// <summary>
-    /// Represents builder registration service for NUnit-based integration tests.
+    /// Represents builder registration service for xUnit.net-based integration tests.
     /// </summary>
     /// <seealso cref="StepsBase" />
     /// <seealso cref="IBuilderRegistrationService" />
